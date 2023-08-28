68676af23b1582904bfde42b8d618930edc1a2d2
Boris Vinnik <vinniklawyer@yandex.ru>
2023-08-28 15:09:43 +0800

    Create project

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+require("hardhat-gas-reporter");
+
+module.exports = {
+  defaultNetwork: "sepolya",
+  networks: {
+    hardhat: {},
+    ethers: {
+      url: process.env.INFURA_URL_ETH,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    sepolya: {
+      url: process.env.INFURA_URL_SEPOLYA,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mumbai: {
+      url: process.env.MUMBAI_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    bsc: {
+      url: process.env.BSC_TESTNET_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 4,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
